56391d770816a1e6076b686de1552af0c87e0a9c
Ayush2020012016 <ayush.2020012016@tulas.edu.in>
2021-07-09 11:20:41 +0530

    commited

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
      <div className="header">
         <div className="firstpart">
             <ul className="listheader">
-                <li className="firstinlist"><Link to={process.env.PUBLIC_URL + '/Web_Inspiration'}>WebInspiration</Link> </li>
+                <li className="firstinlist"><Link to={process.env.PUBLIC_URL + '/'}>WebInspiration</Link> </li>
 
             </ul>
         </div>
